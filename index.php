1b19b22dbe53c9ba7ff2c8beae24cf1c55098520
Eric Thomas <edt514@gmail.com>
2021-05-23 15:56:20 -0400

    Add project assets

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Eric Thomas">
+	<meta name="description" content="Eric Thomas recreation of the DevChallenges Gallery challenge">
+	<title> My Gallery </title>
+	<link rel="shortcut icon" href="img/icon.png">    
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/gallery.css">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+   
+</body>
+</html>

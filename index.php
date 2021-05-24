72ff77cfe79fae55a8dac0a5345d1ddbcff6cea4
Eric Thomas <edt514@gmail.com>
2021-05-23 23:28:57 -0400

    Update info and gallery to use CSS grid as a mobile solution

--- index.php
+++ index.php
@@ -27,77 +27,39 @@
         </div>
     </section>
     
-    <section class="container">
-        <div class="row center-text">
-            <div class="col-sm-12 col-md-3"> &nbsp; </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="row row-cols-3">
-                    <div class="col-4">
-                        <p class="info">
-                            <span> 100 </span>
-                            <br>
-                            <span> Posts </span>
-                        </p>
-                    </div>
-                    <div class="col-4">
-                        <p class="info">
-                            <span> 2,242 </span>
-                            <br>
-                            <span> Followers </span>
-                        </p>
-                    </div>
-                    <div class="col-4">
-                        <p class="info">
-                            <span> 1,432 </span>
-                            <br>
-                            <span> Following </span>
-                        </p>
-                    </div>
-                </div>
+    <section class="container center-text">
+        <div class="row">
+            <div class="col"> &nbsp; </div>
+            <div class="col-sm-12 col-md-6 stats">
+                <p class="info">
+                    <span> 100 </span>
+                    <br>
+                    <span> Posts </span>
+                </p>
+                <p class="info">
+                    <span> 2,242 </span>
+                    <br>
+                    <span> Followers </span>
+                </p>
+                <p class="info">
+                    <span> 1,432 </span>
+                    <br>
+                    <span> Following </span>
+                </p>
             </div>
-            <div class="col-sm-12 col-md-3"> &nbsp; </div>
+            <div class="col"> &nbsp; </div>
         </div>
     </section>
         
     <section class="container gallery">
-        <div class="row">
-            <div class="col"> &nbsp; </div>
-            <div class="col-sm-12 col-lg-10">
-                <div class="row">
-                    <div class="col-sm-12 col-md-4">
-                        <img src="img/couch-1.png" alt="Grey couch with a potted plant on a white table">
-                    </div>
-                    <div class="col-sm-12 col-md-8">
-                        <img src="img/couch-2.png" alt="Black couch">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="row">
-                        <div class="col-sm-8">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <img src="img/tub.png" alt="White porcelain bathtub">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <img src="img/bonzai.png" alt="Bonzai tree">
-                                </div>
-                                <div class="col-sm-6">
-                                    <img src="img/plant.png" alt="Plant">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-4">
-                            <img src="img/lamp.png" alt="Black lamp" class="xl">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col"> &nbsp; </div>
-        </div>
+        <img src="img/couch-1.png" alt="Grey couch with a potted plant on a white table">
+        <img src="img/couch-2.png" alt="Black couch">
+        <img src="img/tub.png" alt="White porcelain bathtub">
+        <img src="img/bonzai.png" alt="Bonzai tree">
+        <img src="img/plant.png" alt="Plant">
+        <img src="img/lamp.png" alt="Black lamp">
     </section>
-    
+        
     <div class="clear"> &nbsp; </div>
     <div class="clear"> &nbsp; </div>
     

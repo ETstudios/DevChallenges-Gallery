4083ecf714471292a2dadb708443b7ab640703ca
Eric Thomas <edt514@gmail.com>
2021-05-23 20:57:12 -0400

    Add stats and profile sections

--- index.php
+++ index.php
@@ -12,6 +12,57 @@
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-   
+    <section class="container profile">
+        <div class="center-text">
+            <img src="img/profile.png" alt="Rodney Cotton profile photo" class="profile">
+        </div>
+        <div class="row">
+            <div class="col-12 center-text">
+                <p>
+                    Rodney Cotton
+                    <br>
+                    <em> Helsinki, Finland </em>
+                </p>
+            </div>
+        </div>
+    </section>
+    
+    <section class="container">
+        <div class="row center-text">
+            <div class="col-sm-12 col-md-3"> &nbsp; </div>
+            <div class="col-sm-12 col-md-6">
+                <div class="row row-cols-3">
+                    <div class="col-4">
+                        <p class="info">
+                            <span> 100 </span>
+                            <br>
+                            <span> Posts </span>
+                        </p>
+                    </div>
+                    <div class="col-4">
+                        <p class="info">
+                            <span> 2,242 </span>
+                            <br>
+                            <span> Followers </span>
+                        </p>
+                    </div>
+                    <div class="col-4">
+                        <p class="info">
+                            <span> 1,432 </span>
+                            <br>
+                            <span> Following </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-3"> &nbsp; </div>
+        </div>
+    </section>
+    
+    <div class="clear"> &nbsp; </div>
+    
+    <section class="container">
+    
+    </section>
 </body>
 </html>
